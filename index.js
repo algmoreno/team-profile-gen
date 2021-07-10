84e87dd2d33b29360839bf89130e89cd7f73032e
algmoreno <alg.moreno00@gmail.com>
2021-07-10 13:59:29 -0700

    employee info landing on html

--- index.js
+++ index.js
@@ -1,98 +1,151 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const genIndex = require('./utils/generateIndex.html');
+const genIndex = require('./utils/generateIndex.js');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
-const employeeQuestions = [
+const teamArray = [];
+// create two empty arrays stored in variables, within create employee function ask
+//questions. .then for new instances. Push new employee to team array, id to id array 
+
+const employeeType = () => {
+    return inquirer.prompt([
     {
-    type: 'text',
+    type: 'list',
     name: 'employeeType',
-    message: "What position is the new employee?",
+    message: "What position is the new employee?", 
     choices: [
-        "Manager",
-        "Engineer",
-        "Intern"
-        ]
-    },
-    {
-    type: 'text',
-    name: 'employeeName',
-    message: "What is the employee's name"
-    },
-    {
-    type: 'text',
-    name: 'employeeID',
-    message: "Enter employee id"
-    },
-    {
-    type: 'text',
-    name: 'employeeEmail',
-    message: "Enter employee's email"
+                "Manager",
+                "Engineer",
+                "Intern"
+                ]
     }
-]
+    ])
+    .then((data) => {
+        if (data.employeeType === 'Manager' || data.employeeType === 'manager') {
+            promptManager(); 
+        }
+        if (data.employeeType === 'Engineer' || data.employeeType === 'engineer') {
+            promptEngineer(); 
+        }
+        if (data.employeeType === 'Intern' || data.employeeType === 'intern') {
+            promptIntern();
+        }
+    })
+    
+}
 
-const managerQuestion = [
+const promptManager = () => {
+    return inquirer.prompt([
+    {
+    type: 'text',
+    name: 'name',
+    message: "What is the manager's name"
+    },
+    {
+    type: 'text',
+    name: 'id',
+    message: "Enter manager's id"
+    },
+    {
+    type: 'text',
+    name: 'email',
+    message: "Enter manager's email"
+    },
     {
     type: 'text',
     name: 'officeNum',
     message: "Enter manager's office number"
-    }
-]
+    },
+    ])
+    .then((managerData) => {
+        verifyEmployee(managerData)
+    })
+}
 
-const engineerQuestion = [
+const promptEngineer = () => {
+    return inquirer.prompt([
+    {
+    type: 'text',
+    name: 'name',
+    message: "What is the engineer's name"
+    },
+    {
+    type: 'text',
+    name: 'id',
+    message: "Enter engineer's id"
+    },
+    {
+    type: 'text',
+    name: 'email',
+    message: "Enter engineer's email"
+    },
     {
     type: 'text',
     name: 'github',
     message: "Entern github username"
     }
-]
+    ])
+    .then((engineerData) => {
+        verifyEmployee(engineerData)
+    })
+}
 
-const internQuestion = [
+const promptIntern = () => {
+    return inquirer.prompt([
+    {
+    type: 'text',
+    name: 'name',
+    message: "What is the intern's name"
+    },
+    {
+    type: 'text',
+    name: 'id',
+    message: "Enter intern's id"
+    },
+    {
+    type: 'text',
+    name: 'email',
+    message: "Enter intern's email"
+    },
     {
     type: 'text',
     name: 'school',
-    message: "Entern intern's school"
+    message: "Enter intern's school"
     }
-]
-
-function init() {
-    inquirer.prompt(employeeQuestions).then((data) => {
-        if (data.employeeType === 'Manager' || data.employeeType === 'manager') {
-            managerAsk(); 
-        }
-        if (data.employeeType === 'Engineer' || data.employeeType === 'engineer') {
-            engineerAsk(); 
-        }
-        if (data.employeeType === 'Intern' || data.employeeType === 'intern') {
-            internAsk(); 
-        }
+    ])
+    .then((internData) => {
+        verifyEmployee(internData)
     })
 }
 
-function managerAsk() {
-    inquirer.prompt(managerQuestion).then((managerData) => {
-        console.log(managerData.officeNum)
-    })
+function verifyEmployee(data) {
+    if (data.officeNum) {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNum)
+        teamArray.push(manager)
+        writeFile(manager);
+    }
+    else if (data.github) {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github)
+        teamArray.push(engineer);
+        writeFile(engineer);
+    }
+    else if (data.school) {
+        const intern = new Intern(data.name, data.id, data.email, data.school)
+        teamArray.push(intern)
+        writeFile(intern);
+    }
 }
 
-function engineerAsk() {
-    inquirer.prompt(engineerQuestion).then((engineerData) => {
-        console.log(engineerData.github)
-    })
-}
-
-function internAsk() {
-    inquirer.prompt(internQuestion).then((internData) => {
-        console.log(internData.school)
-    })
-}
-
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, genIndex(data), function (err) {
+function writeFile(employee) {
+    fs.writeFile('./index.html', genIndex(employee), function (err) {
         if (err) {
             return console.log(err);
         }
+        
     })
+    
 }
 
-
-init();
+employeeType(); 
---
+++ utils/generateIndex.js
@@ -0,0 +1,24 @@
+function genIndex(employee) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+</head>
+<body>
+    <nav>
+        <h1>My Team</h1>
+    </nav>
+<content>
+${employee.name}
+</content>
+</body>
+</html>
+`
+}
+
+module.exports = genIndex; 
+    
